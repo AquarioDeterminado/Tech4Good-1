a502e82f83555b95f9441c19370c087ba49dfd0c
Ri787878 <114110536+Ri787878@users.noreply.github.com>
2024-04-06 05:14:44 +0100

    ice surface

--- Assets/Scripts/Slide.cs
+++ Assets/Scripts/Slide.cs
@@ -6,7 +6,10 @@ public class Slide : MonoBehaviour
 {
 
     [SerializeField] private float defaultSpeed = 0.5f;
+    private bool maintainGround = true;
     private Rigidbody2D _rigidbody;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +20,32 @@ public class Slide : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_rigidbody.velocity.x < defaultSpeed)
+
+        if (maintainGround)
+        {
+            RaycastHit hitFloor;
+            RaycastHit hitWall;
+            Physics.Raycast(transform.position, transform.up * -1, out hitFloor);
+            Physics.Raycast(transform.position, transform.right, out hitWall);
+            if (hitWall.transform)
+            {
+                Debug.Log("Found a wall will try to rotate away from wall");
+                transform.forward = hitWall.normal * _rigidbody.velocity.magnitude;
+            }
+            else if (hitFloor.transform)
+            {
+                transform.position = hitFloor.point * _rigidbody.velocity.magnitude;
+                Debug.Log("Found a collider underneath!");
+            }
+        }
+
+        /*if (_rigidbody.velocity.x < defaultSpeed)
         {
             var right = transform.right;
-            var foward  = new Vector2(right.x, right.z).normalized;
-            Vector2 velocity =  (new Vector2(foward.x, foward.y * 1.5f)) * defaultSpeed;
+            var foward = new Vector2(right.x, right.z).normalized;
+            Vector2 velocity = (new Vector2(foward.x, foward.y 1.5f)) * defaultSpeed;
             _rigidbody.velocity = velocity;
         }
+        */
     }
 }

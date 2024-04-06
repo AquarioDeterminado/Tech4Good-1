f0e409630f597c77705eb8230ecff35fc765aa42
[Aquario Determinado] <aquario.determinado@gmail.com>
2024-04-06 03:11:45 +0100

    Scripts created;

--- Assets/Scripts/Slide.cs
+++ Assets/Scripts/Slide.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class Slide : MonoBehaviour
 {
-    
-    
+
+    [SerializeField] private float defaultSpeed = 0.5f;
+    private Rigidbody2D _rigidbody;
+
     // Start is called before the first frame update
-    
-    
     void Start()
     {
-        
+        _rigidbody = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_rigidbody.velocity.x < defaultSpeed)
+        {
+            var right = transform.right;
+            var foward  = new Vector2(right.x, right.z).normalized;
+            Vector2 velocity =  (new Vector2(foward.x, foward.y * 1.5f)) * defaultSpeed;
+            _rigidbody.velocity = velocity;
+        }
     }
 }
--- Assets/Scripts/UserDownImpulse.cs
+++ Assets/Scripts/UserDownImpulse.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class UserDownImpulse : MonoBehaviour
 {
 
-    [SerializeField] private KeyCode playerDown = KeyCode.Space;
-    private Rigidbody2D 
+    [SerializeField] private KeyCode keyCode = KeyCode.Space;
+    [SerializeField] private float forceIntensity = 0.5f;
+    private Rigidbody2D _rigidbody2D;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody2D = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(keyCode))
+        {
+            _rigidbody2D.AddForce(new Vector2(0, -1 * forceIntensity));
+        }
     }
 }

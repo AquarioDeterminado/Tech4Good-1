f42b06637b6183c106cf03a3b278ea8118863cc0
[Aquario Determinado] <aquario.determinado@gmail.com>
2024-04-06 02:32:30 +0100

    Scripts created;

---
+++ Assets/Scripts/UserDownImpulse.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserDownImpulse : MonoBehaviour
+{
+
+    [SerializeField] private KeyCode playerDown = KeyCode.Space;
+    private Rigidbody2D 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
